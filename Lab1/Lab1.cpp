445206a599c14063fe9e78876c8c93f61494f543
Nahagliivv <Rebesh.mark@gmail.com>
2020-09-07 20:36:15 +0300

    first commit

--- Lab1/Lab1.cpp
+++ Lab1/Lab1.cpp
@@ -2,11 +2,15 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+  
+	std::cout << "Hello World!\n";
+	cout << "Hello World!\n";
 }
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
